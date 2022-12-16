cf1beabee7bdc43f3d5bbfb0dd340b85251600a6
Paul Suh <plsuh@goodeast.com>
2022-12-16 10:39:32 -0500

    Clean up Day 15 part 2

--- Day-15/BeaconExclusionZone2.py
+++ Day-15/BeaconExclusionZone2.py
@@ -2,51 +2,59 @@ import fileinput
 import re
 
 # row_blocks = [[] for i in range(21)]
-row_blocks = [[] for i in range(4000001)]
+# row_blocks = [[] for i in range(4000001)]
+
+sensors = []
 
 for raw_line in fileinput.input():
 
     matches = re.match(r"Sensor at x=([\-0-9]+), y=([\-0-9]+): closest beacon is at x=([\-0-9]+), y=([\-0-9]+)", raw_line)
-    sensor_x = int(matches.group(1))
-    sensor_y = int(matches.group(2))
-    beacon_x = int(matches.group(3))
-    beacon_y = int(matches.group(4))
+    sensors.append({
+        "sensor_x": int(matches.group(1)),
+        "sensor_y": int(matches.group(2)),
+        "beacon_x": int(matches.group(3)),
+        "beacon_y": int(matches.group(4)),
+        "distance": abs(int(matches.group(1)) - int(matches.group(3))) + \
+                    abs(int(matches.group(2)) - int(matches.group(4)))
+    })
 
-    distance = abs(sensor_x - beacon_x) + \
-               abs(sensor_y - beacon_y)
+# for row_index in range(21):
+for row_index in range(4000001):
 
-    for row_index, one_row in zip(range(len(row_blocks)), row_blocks):
+    if row_index % 10000 == 0:
+        print(f"processing row {row_index}")
 
-        if row_index % 10000 == 0:
-            print(f"input line {fileinput.lineno()} setting up initial blocks {row_index}")
 
-        blocked_out_distance = distance - abs(row_index - sensor_y)
+    one_row = []
+    # calculate the blocked out areas for each sensor for this row
+    for one_sensor in sensors:
+        blocked_out_distance = one_sensor["distance"] - abs(row_index - one_sensor["sensor_y"])
         if blocked_out_distance < 0:
             # no block in this row, skip it
             continue
 
-        current_block_start = sensor_x - blocked_out_distance
-        current_block_end = sensor_x + blocked_out_distance + 1     # it's one past the end
+        current_block_start = one_sensor["sensor_x"] - blocked_out_distance
+        current_block_end = one_sensor["sensor_x"] + blocked_out_distance + 1     # it's one past the end
 
         one_row.append([current_block_start, current_block_end])
 
-melded_rows = []
-for index, one_row in zip(range(len(row_blocks)), row_blocks):
     # sort each row by the starting block numbers
     one_row.sort(key=lambda b: b[0])
     # print(one_row)
 
     # meld the blocks
-    new_row = []
+    melded_row = []
+    current_block = []
     for one_block in one_row:
 
-        if len(new_row) == 0:
-            new_row.append(one_block)
+        if len(melded_row) == 0:
+            melded_row.append(one_block)
             current_block = one_block
+            continue
 
         elif one_block[0] > current_block[1]:
             # new block start
-            new_row.append(one_block)
+            melded_row.append(one_block)
             current_block = one_block
             continue
 
@@ -60,17 +68,23 @@ for index, one_row in zip(range(len(row_blocks)), row_blocks):
             current_block[1] = one_block[1]
 
     # if this is a new block, append
-    if new_row[-1][1] < current_block[0]:
-        new_row.append(current_block)
+    if melded_row[-1][1] < current_block[0]:
+        melded_row.append(current_block)
     # print(index, new_row)
 
-    if index % 10000 == 0:
-        print(f"melding blocks row {index}")
+    if melded_row[0][0] > 0:
+        print(f"found {melded_row}")
+        print(row_index, melded_row[0][0]-1, (melded_row[0][0]-1)*4000000+row_index)
+        break
 
-    if new_row[0][0] > 0 or \
-        new_row[-1][1] < 4000001 or \
-        len(new_row) > 1:
+    # elif melded_row[-1][1] < 21:
+    elif melded_row[-1][1] < 4000001:
+        print(f"found {melded_row}")
+        print(row_index, melded_row[-1][1], (melded_row[-1][1])*4000000+row_index)
+        break
 
-        print(index, new_row)
+    elif len(melded_row) > 1:
+        print(f"found {melded_row}")
+        print(row_index, melded_row[0][1], (melded_row[0][1])*4000000+row_index)
         break
 
